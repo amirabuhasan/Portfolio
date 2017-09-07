306520ff38d4ad3cb434ba4044f1ba7165955be9
amirabuhasan <amirabuhasan@gmail.com>
2017-09-07 20:10:35 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,19 +1,32 @@
 
-
-
-function navbar(element){
-    $(".nav li").removeClass("active");
-    $(element).toggleClass("active");
-};
+//
+//
+// function navbar(element){
+//     $(".nav li").removeClass("active");
+//     $(element).toggleClass("active");
+// };
+//
+// function tryDocument(){
+//   $(".nav li").on("click", function(){
+//     $(navbar(this));
+//   });
+//   $(".portfolio").on("click", function(){
+//     $(".nav li").removeClass("active");
+//   });
+// };
+//
+//
+// $(tryDocument());
 
 function tryDocument(){
-  $(".nav li").on("click", function(){
-    $(navbar(this));
+  $(".memory-game").on("click", function(){
+    console.log("I am clicked");
+    $('#memoryGameModal').modal('show');
   });
-  $(".portfolio").on("click", function(){
-    $(".nav li").removeClass("active");
+  $(".movie-website").on("click", function(){
+    console.log("I am clicked");
+    $('#movieWebsiteModal').modal('show');
   });
 };
-
 
 $(tryDocument());
